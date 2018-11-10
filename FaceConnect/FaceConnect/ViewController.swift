8620daec2e59c01d75909d6de3367ae85b9c70fb
davidwc2008 <davidwc2008@gmail.com>
2018-11-10 17:18:28 -0500

    Add files via upload

---
+++ FaceConnect/FaceConnect/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  FaceConnect
+//
+//  Created by David Chen on 11/10/18.
+//  Copyright © 2018 David Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+}
+
